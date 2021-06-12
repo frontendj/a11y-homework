6736851f320a6ea0fc28250f2921b9799ca39101
Dmitrii Kuznetsov <dk@bayda.ru>
2021-06-12 13:54:37 +0100

    generated website

--- src/_data/isdevelopment.js
+++
@@ -1,3 +0,0 @@
-module.exports = function() {
-    return /serve/.test(process.argv.join());
-};
--- src/_data/metadata.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    title: 'Пушкинский музей',
-    url: 'http://frontendj.ru/pushkin',
-    language: 'ru',
-    description: 'Очень интересный музей в центре Москвы',
-    ogImage: 'http://frontendj.ru/pushkin/images/og.jpg'
-};

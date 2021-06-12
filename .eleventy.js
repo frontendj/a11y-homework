f296900316f691cc099b8b736d4aec528cdd77a2
Dmitrii Kuznetsov <dk@bayda.ru>
2021-06-12 18:43:35 +0100

    more styling

--- .eleventy.js
+++ .eleventy.js
@@ -43,6 +43,10 @@ module.exports = function(eleventyConfig) {
         return `${monthName} ${date}, ${year}`;
     });
 
+    eleventyConfig.addFilter('encodeUri', function(string) {
+        return encodeURIComponent(string);
+    });
+
     // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');

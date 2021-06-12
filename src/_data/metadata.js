0c470ddbd94a458269b3322776e25af83430252d
Dmitrii Kuznetsov <dk@bayda.ru>
2021-06-12 22:08:34 +0100

    styling and bugs

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,6 +1,8 @@
 module.exports = {
     title: 'Пушкинский музей',
+    titleLong: 'Государственный музей изобразительных искусств имени А. С. Пушкина',
     eng: {
         title: 'Pushkin museum',
+        titleLong: 'Pushkin state museum of arts',
     }
 };
